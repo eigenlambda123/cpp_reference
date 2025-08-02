e2aa7504be87b33ba9bea468115787a4cfabf605
eigenlambda123 <rmvilla987@gmail.com>
2025-08-02 16:10:05 +0800

    1.3b_object_and_variables

---
+++ 01_basics/1.3_into_to_objects_and_variable/1.3b_object_and_variables/1.3b_object_and_variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int x; // define a variable (object with a name) of type int
+    x = 42; // store a value in the object x
+
+    std::cout << "The value of x is: " << x << std::endl; // access and print the value
+
+    return 0;
+}
