b2c8a750e0f92d31eb6fac01197019ca8010a76a
eigenlambda123 <rmvilla987@gmail.com>
2025-08-04 23:56:22 +0800

    1.5a_std_cout

---
+++ 01_basics/1.5_iostream_cout_cin_and_endl/1.5a_std_cout/1.5_std_cout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World";
+    std::cout << 4;
+
+    int x{ 5 }; // define integer variable x, initialized with value 5
+    std::cout << x; // print value of x (5) to console
+
+    std::cout << "Hello" << "World!";
+    std::cout << "x is equal to: " << x;
+
+    return 0;
+}
