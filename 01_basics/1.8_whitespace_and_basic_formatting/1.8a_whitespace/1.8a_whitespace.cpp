5681ba630faf4236dd0396b918c93f8ec3187627
eigenlambda123 <rmvilla987@gmail.com>
2025-08-05 08:20:41 +0800

    1.8a_whitespace

---
+++ 01_basics/1.8_whitespace_and_basic_formatting/1.8a_whitespace/1.8a_whitespace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int    main   ( )   // Extra spaces: valid syntax
+{
+    int   a    =    5   ;
+    int   b=10;
+
+    std::cout << "Sum     is: " << a + b << '\n'; // Output: Sum     is: 15
+
+    return 0;
+}
