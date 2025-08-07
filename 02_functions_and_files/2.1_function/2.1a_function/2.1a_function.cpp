bbea005e883ee91e82e572ccf92dc8905a3d3f1d
eigenlambda123 <rmvilla987@gmail.com>
2025-08-07 09:19:32 +0800

    2.1a_function

---
+++ 02_functions_and_files/2.1_function/2.1a_function/2.1a_function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Function declaration
+void greetUser() {
+    std::cout << "Hello, user!\n";
+}
+
+int main() {
+    std::cout << "Program started.\n";
+
+    // Function call
+    greetUser();
+
+    std::cout << "Program ended.\n";
+
+    return 0;
+}
