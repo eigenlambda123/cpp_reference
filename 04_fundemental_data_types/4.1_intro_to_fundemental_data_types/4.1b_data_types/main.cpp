d51a1507f6fb3df9af54eb8c9a37594539b4c7aa
eigenlambda123 <rmvilla987@gmail.com>
2025-08-30 14:44:56 +0800

    4.1b_data_types

---
+++ 04_fundemental_data_types/4.1_intro_to_fundemental_data_types/4.1b_data_types/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 65;       // integer
+    char ch = 'A';      // character
+    double pi = 3.1415; // floating point
+    bool flag = true;   // boolean
+
+    cout << "int: " << num << endl;
+    cout << "char: " << ch << endl;
+    cout << "double: " << pi << endl;
+    cout << "bool: " << flag << endl;
+}
